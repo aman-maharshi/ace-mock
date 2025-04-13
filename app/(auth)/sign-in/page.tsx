c3f7b050f08ec51ea29a5bcc0d9b4c20cf03bd53
aman-maharshi <amanm4harshi@gmail.com>
2025-04-13 17:17:17 +0530

    AuthForm init

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,12 @@
+import AuthForm from '@/app/components/AuthForm'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div>
+      <div>SignIn</div>
+      <AuthForm type="sign-in" />
+    </div>
   )
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,12 @@
+import AuthForm from '@/app/components/AuthForm'
 import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div>
+      <div>SignUp</div>
+      <AuthForm type="sign-up" />
+    </div>
   )
 }
 
