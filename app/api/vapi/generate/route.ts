36fbf7317416ceb041db630b2436b3150b34323a
aman-maharshi <amanm4harshi@gmail.com>
2025-05-04 14:09:26 +0530

    generate questions api updates

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,8 +1,67 @@
-export async function GET(request: Request) {
-  return Response.json({
-    succuess: true,
-    data: 'Thank you for your request',
-  }, {
-    status: 200
-  })
+import { generateText } from "ai"
+import { google } from "@ai-sdk/google"
+import { db } from "@/firebase/admin"
+import { getRandomInterviewCover } from "@/lib/utils"
+
+// generate interview questions from gemini that will be used by vapi agent & storing them in firebase 
+export const POST = async (request: Request) => {
+  const { type, role, level, techstack, amount, userid } = await request.json();
+
+  try {
+    const { text: questions } = await generateText({
+      model: google("gemini-2.0-flash-001"),
+      prompt: `Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
+        
+        Thank you! <3
+    `,
+    })
+
+    const interview = {
+      role: role,
+      type: type,
+      level: level,
+      techstack: techstack.split(","),
+      questions: JSON.parse(questions),
+      userId: userid,
+      finalized: true,
+      coverImage: getRandomInterviewCover(),
+      createdAt: new Date().toISOString(),
+    };
+
+    await db.collection("interviews").add(interview)
+
+    return Response.json(
+      { success: true },
+      { status: 200 }
+    )
+
+  } catch (error) {
+    console.error('Error:', error)
+    return Response.json(
+      {
+        success: false,
+        error: "An error occurred while generating the interview questions.",
+      },
+      { status: 500 }
+    )
+  }
+}
+
+export const GET = async () => {
+  return Response.json(
+    {
+      succuess: true,
+      data: 'Thank you!',
+    },
+    { status: 200 }
+  )
 }
