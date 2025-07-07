ada45443f7a03873d9d8f214de64f0754d58d142
aman-maharshi <amanm4harshi@gmail.com>
2025-07-07 05:57:12 +0530

    tech icon fixes

--- app/components/TechIcons.tsx
+++ app/components/TechIcons.tsx
@@ -1,22 +1,22 @@
-import { getTechLogos } from '@/lib/utils'
-import clsx from 'clsx'
-import Image from 'next/image'
+import { getTechLogos } from "@/lib/utils"
+import clsx from "clsx"
+import Image from "next/image"
 
 const TechIcons = async ({ techStack }: TechIconProps) => {
   const techIcons = await getTechLogos(techStack)
 
   return (
-    <div className='flex flex-row'>
+    <div className="flex flex-row gap-2">
       {techIcons.slice(0, 3).map((icon, index) => {
         const { tech, url } = icon
 
         return (
-          <div key={index} className={clsx(
-            'relative group bg-gray-200 dark:bg-gray-700 rounded-full p-2 flex-center',
-            index > 0 &&"-ml-2"
-          )}>
-            <span className='tech-tooltip'>{tech}</span>
-            <Image src={url} alt={tech} width={100} height={100} className='size-5' />
+          <div
+            key={index}
+            className="relative group bg-gray-200 dark:bg-gray-700 rounded-full p-2 flex-center hover:bg-gray-300 dark:hover:bg-gray-600 transition-colors"
+          >
+            <span className="tech-tooltip">{tech}</span>
+            <Image src={url} alt={tech} width={100} height={100} className="size-5" />
           </div>
         )
       })}
